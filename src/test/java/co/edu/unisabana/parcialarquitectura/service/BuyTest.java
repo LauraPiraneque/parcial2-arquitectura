398d51b65aad0ffbcbbc4bfe9c84b1f345a06724
Laura Piraneque <laurapiraneque14@gmail.com>
2022-10-04 07:58:39 -0500

    Dependency injection
    Dependency injection has been added to the Buy class, through the constructor.

--- src/test/java/co/edu/unisabana/parcialarquitectura/service/BuyTest.java
+++ src/test/java/co/edu/unisabana/parcialarquitectura/service/BuyTest.java
@@ -1,10 +1,11 @@
 package co.edu.unisabana.parcialarquitectura.service;
 
+import co.edu.unisabana.parcialarquitectura.repository.Database;
 import org.junit.jupiter.api.Test;
 
 class BuyTest {
 
-  private final Buy service = new Buy();
+  private final Buy service = new Buy(new Database());
 
   @Test
   public void test() {
